6a8a46d85ad449289fd9ca9cd0fc71966414a570
Kevin Ernesto Meza Garcia <erasmomeza11@gmail.com>
2026-05-19 23:17:02 -0600

    Kevin Meza - ejercicio2 imprime numeros pares

--- src/ejercicio2.java
+++ src/ejercicio2.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ejercicio2 {
-}
+
+        public static void main(String[] args) {
+
+            Scanner entrada = new Scanner(System.in);
+
+            int numero;
+            int contador = 0;
+
+            System.out.println("--- Sistema de Números Pares ---");
+
+            System.out.print("Ingresa un número límite: ");
+            numero = entrada.nextInt();
+
+            System.out.println("\nNúmeros pares encontrados:");
+
+            while (contador <= numero) {
+
+                if (contador % 2 == 0) {
+                    System.out.println("✔ Número par: " + contador);
+                }
+
+                contador++;
+            }
+
+            System.out.println("\n--- Proceso Finalizado ---");
+
+            entrada.close();
+        }
+    }
