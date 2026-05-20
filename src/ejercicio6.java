933103f6173cfb34b20a14dd92433d51a67263ba
Kevin Ernesto Meza Garcia <erasmomeza11@gmail.com>
2026-05-19 23:24:21 -0600

    Kevin Meza - ejercicio6

---
+++ src/ejercicio6.java
@@ -0,0 +1,16 @@
+public class ejercicio6 {
+
+    public static void main(String[] args) {
+
+        System.out.println("--- Primeros 50 Múltiplos de 5 ---");
+
+        for (int i = 1; i <= 50; i++) {
+
+            int resultado = i * 5;
+
+            System.out.println("5 x " + i + " = " + resultado);
+        }
+
+        System.out.println("\n--- Proceso Finalizado ---");
+    }
+}
